59cf25f6695a0b9dcca475a041bbbf2b1c59fb15
Pat Hawks <patrick-hawks@uiowa.edu>
2017-04-24 17:03:06 -0500

    Add dummy heuristic

---
+++ heuristics.scala
@@ -0,0 +1,8 @@
+/*==================================================
+    CS:4420 Artificial Intelligence
+    Spring 2017
+    Heuristics
+    Name: Pat Hawks, Ryan Larson, Rui Yang
+  ==================================================*/
+
+def dummy(t:Any) = {0}
