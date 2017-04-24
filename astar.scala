48426b233ec2c9f629e51146086389eed1ae3936
Pat Hawks <patrick-hawks@uiowa.edu>
2017-04-24 13:15:48 -0500

    First draft of A* search

---
+++ astar.scala
@@ -0,0 +1,38 @@
+/*==================================================
+    CS:4420 Artificial Intelligence
+    Spring 2017
+    Project
+    Name: Pat Hawks, Ryan Larson, Rui Yang
+  ==================================================*/
+
+import scala.collection.mutable.PriorityQueue
+
+/**
+ * A* search
+ * @param a some initial state
+ * @param children function that returns a list of all children for a given
+ *        node, as well as the step to get from current node to that child
+ * @param heuristic function that analyzes the desirability of the current state
+ */
+def astar[T, S] (a:T, children:T=>List[(T, S)], heuristic:T=>Int):List[S] = {
+  val fringe = PriorityQueue.empty[(Int, T, List[S])](
+    Ordering.by((_: (Int, T, List[S]))._1).reverse
+  )
+
+  def search(a:T, currentDepth:Int, solution:List[S]):List[S] = {
+    children(a).foreach {
+      case (a, s) => {
+        val score = currentDepth + heuristic(a)
+        val newSolution = solution ++ List(s)
+        val t = (score, a, newSolution)
+        fringe += t
+      }
+    }
+    fringe.dequeue() match {
+      case (0, _, solution) => solution
+      case (_, a, solution) => search(a, currentDepth + 1, solution)
+    }
+  }
+
+  search(a, 0, Nil)
+}
