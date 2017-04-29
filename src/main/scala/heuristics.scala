defb4091a939e53cda2a1e25983125967f0ec3ff
Pat Hawks <patrick-hawks@uiowa.edu>
2017-04-29 00:13:29 -0500

    Move heuristics to heuristics.scala

--- src/main/scala/heuristics.scala
+++ src/main/scala/heuristics.scala
@@ -6,3 +6,39 @@
   ==================================================*/
 
 def dummy(t:Any) = {0}
+
+// Manhatta distance heuristc function
+def Manhatta(s:Int, tiles:Map[Pos,Tile]):Int={
+     var sum=0
+     for (pair<-tiles){
+         val ((row,col),tile)=pair
+         val goalRow=ceil(1.0*tile/s)
+         val goalCol=tile-(goalRow-1)*s
+         sum+=abs(row-goalRow.toInt)
+         sum+=abs(col-goalCol.toInt)}
+     sum
+}
+
+// Linear-conflict heuristic function
+def linearConflict(s:Int, tiles:Map[Pos,Tile]):Int={
+    var sum= Manhatta(s,tiles)
+    def pos(pair:((Int,Int),Int))={
+        val ((r,c),t)=pair;
+        var gr=ceil(1.0*t/s)
+        (r,c,t,gr,t-(gr-1)*s)
+    }
+    for (pair<-tiles){
+        val (row,col,tile,goalRow,goalCol)=pos(pair)
+        if (row==goalRow){
+            for (sameRow<-tiles.filterKeys(k=>(k._1==row & k._2<col))){
+                val (row2,col2,tile2,goalRow2,goalCol2)=pos(sameRow)
+                if (goalRow2==row & (col2-col)*(goalCol2-goalCol)<0) sum+=2}
+        }
+        if (col==goalCol){
+            for (sameCol<-tiles.filterKeys(k=>(k._1<row & k._2==col))){
+                val (row2,col2,tile2,goalRow2,goalCol2)=pos(sameCol)
+                if (goalCol2==col & (row2-row)*(goalRow2-goalRow)<0) sum+=2}
+        }
+    }
+    sum
+}
--- src/main/scala/n-square.scala
+++ src/main/scala/n-square.scala
@@ -118,7 +118,7 @@ case object Right extends Operator {
 
 // Up operator
 case object Up extends Operator {
-  // the apply method returns  
+  // the apply method returns
   override def apply (s: State): Option[State] =
   s match {
     case State(_, (1, _)) => None
@@ -129,7 +129,7 @@ case object Up extends Operator {
   }
 }
 
-// Down operatorcase 
+// Down operatorcase
 object Down extends Operator {
   // the apply method returns
   override def apply (s: State): Option[State] =
@@ -182,39 +182,3 @@ def toBoard(cur:State): (Int,Map[Pos,Tile]) ={
     cur match{
     case State(b,e)=> b match{
     case Board(s,t)=> (s, t) }}}
-
-// Manhatta distance heuristc function
-def Manhatta(s:Int, tiles:Map[Pos,Tile]):Int={
-     var sum=0
-     for (pair<-tiles){
-         val ((row,col),tile)=pair
-         val goalRow=ceil(1.0*tile/s)
-         val goalCol=tile-(goalRow-1)*s
-         sum+=abs(row-goalRow.toInt)
-         sum+=abs(col-goalCol.toInt)}
-     sum
-}
-
-// Linear-conflict heuristic function
-def linearConflict(s:Int, tiles:Map[Pos,Tile]):Int={
-    var sum= Manhatta(s,tiles)
-    def pos(pair:((Int,Int),Int))={
-        val ((r,c),t)=pair;
-        var gr=ceil(1.0*t/s)
-        (r,c,t,gr,t-(gr-1)*s)
-    }
-    for (pair<-tiles){
-        val (row,col,tile,goalRow,goalCol)=pos(pair)
-        if (row==goalRow){
-            for (sameRow<-tiles.filterKeys(k=>(k._1==row & k._2<col))){
-                val (row2,col2,tile2,goalRow2,goalCol2)=pos(sameRow)
-                if (goalRow2==row & (col2-col)*(goalCol2-goalCol)<0) sum+=2}
-        }
-        if (col==goalCol){
-            for (sameCol<-tiles.filterKeys(k=>(k._1<row & k._2==col))){
-                val (row2,col2,tile2,goalRow2,goalCol2)=pos(sameCol)
-                if (goalCol2==col & (row2-row)*(goalRow2-goalRow)<0) sum+=2}
-        }
-    }
-    sum
-}
