9f588a9301ca8e2d934ddf899d452c5d897b61ec
otokonoko8 <rui-yang@uiowa.edu>
2017-04-28 21:25:44 -0500

    Update n-square.scala

--- src/main/scala/n-square.scala
+++ src/main/scala/n-square.scala
@@ -5,6 +5,7 @@
     Names: Pat Hawks, Ryan Larson, Rui Yang
   ==================================================*/
 import util.control.Breaks._
+import scala.math._
 // A position on the puzzle boad is encoded just as a pair of integers
 // Each coordinate ranges from 1 to n, where n is the n of the puzzle
 // position (1,1) is the top-left position in the board.
@@ -179,16 +180,33 @@ def prepare_trial_board(): State = {
 def toBoard(cur:State): (Int,Map[Pos,Tile]) ={
     cur match{
     case State(b,e)=> b match{
-     case Board(s,t)=> (s, t) }}}
+    case Board(s,t)=> (s, t) }}}
 
 def Manhatta(s:Int, tiles:Map[Pos,Tile]):Int={
      var sum=0
      for (pair<-tiles){
          val ((row,col),tile)=pair
-         val goalRow=scala.math.ceil(1.0*tile/s)
+         val goalRow=ceil(1.0*tile/s)
          val goalCol=tile-(goalRow-1)*s
-         sum+=scala.math.abs(row-goalRow.toInt)
-         sum+=scala.math.abs(col-goalCol.toInt)}
-     sum}
+         sum+=abs(row-goalRow.toInt)
+         sum+=abs(col-goalCol.toInt)}
+     sum
+}
 
-
+def linearConflict(s:Int, tiles:Map[Pos,Tile]):Int={
+    var sum= Manhatta(s,tiles)
+    def pos(pair:((Int,Int),Int))={
+        val ((r,c),t)=pair;
+        var gr=ceil(1.0*t/s)
+        (r,c,t,gr,t-(gr-1)*s)
+    }
+    for (pair<-tiles){
+        val (row,col,tile,goalRow,goalCol)=pos(pair)
+        if (row==goalRow){
+            for (sameRow<-tiles.filterKeys(k=>(k._1==row & k._2<col))){
+                val (row2,col2,tile2,goalRow2,goalCol2)=pos(sameCol)
+                if (goalCol2==col & (row2-row)*(goalRow2-goalRow)<0) sum+=2}
+        }
+    }
+    sum
+}
