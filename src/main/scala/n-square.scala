6bf1f2f0add8660a08fb4255584f9afb2b87d5d8
otokonoko8 <rui-yang@uiowa.edu>
2017-04-28 21:44:59 -0500

    Update n-square.scala

--- src/main/scala/n-square.scala
+++ src/main/scala/n-square.scala
@@ -204,6 +204,11 @@ def linearConflict(s:Int, tiles:Map[Pos,Tile]):Int={
         val (row,col,tile,goalRow,goalCol)=pos(pair)
         if (row==goalRow){
             for (sameRow<-tiles.filterKeys(k=>(k._1==row & k._2<col))){
+                val (row2,col2,tile2,goalRow2,goalCol2)=pos(sameRow)
+                if (goalRow2==row & (col2-col)*(goalCol2-goalCol)<0) sum+=2}
+        }
+        if (col==goalCol){
+            for (sameCol<-tiles.filterKeys(k=>(k._1<row & k._2==col))){
                 val (row2,col2,tile2,goalRow2,goalCol2)=pos(sameCol)
                 if (goalCol2==col & (row2-row)*(goalRow2-goalRow)<0) sum+=2}
         }
