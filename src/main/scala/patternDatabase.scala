e83ceb7e00e80026d1db00ef98486fcd45c7a4aa
otokonoko8 <rui-yang@uiowa.edu>
2017-04-30 00:27:09 -0500

    Update patternDatabase.scala

--- src/main/scala/patternDatabase.scala
+++ src/main/scala/patternDatabase.scala
@@ -3,22 +3,33 @@ import java.io._
 import scala.collection.mutable.PriorityQueue
 import scala.collection.mutable.Set
 
-var patternCost:Map[State,Int]=Map()
+// Fringe Pattern Database 
+var fpdb:Map[State,Int]=Map()// Corner Pattern Database 
+var cpdb:Map[State,Int]=Map()
 
-def generatePDB(initial:State, makeNodes: State =>List[(State,Operator)],maxCost:Int):Unit = {
-  var patternCost:Map[State,Int]=Map()
+// Generate above Pattern Databases
+def getNAPDB(initial:State, makeNodes: State =>List[(State,Operator)],maxCost:Int):Unit = {
   val fringe = PriorityQueue.empty[(Int, State)](
     Ordering.by((_: (Int, State))._1).reverse)
   val seen = Set[State]()
-  var max = 0
 
   def search(a:State, g:Int):Unit = { 
-    println(a)
-    val p=pattern(a)
-    println(p)
-    println(g)
-    if (!(patternCost contains p))  patternCost= patternCost +(p->g)
-    else { if (patternCost(p)>g) patternCost= patternCost +(p->g)}
+    val fp=pattern(a, true)
+    if (!( fpdb contains fp))        
+      fpdb= fpdb +(fp->g)
+    else { 
+      if (fpdb(fp)>g) 
+        fpdb= fpdb +(fp->g)
+    }
+    
+    val cp=pattern(a, false)
+    if (!( cpdb contains cp))  
+      cpdb= cpdb +(cp->g)
+    else { 
+      if (cpdb(cp)>g) 
+        cpdb= cpdb +(cp->g)
+    }
+
     // Find all valid moves from a state, and add them to the fringe
     if (g<maxCost){
     makeNodes(a).foreach {
@@ -26,8 +37,6 @@ def generatePDB(initial:State, makeNodes: State =>List[(State,Operator)],maxCost
         if (!seen(a)) {
           val cost = g + 1
           val node = (cost, a)
-          if (cost > max)
-            max = cost
           fringe += node
           seen += a
         }
@@ -41,27 +50,50 @@ def generatePDB(initial:State, makeNodes: State =>List[(State,Operator)],maxCost
     }
   }
   search(initial, 0)
-  for (i<- patternCost) {println(i._1) ;println(i._2)}
+  
+  /*println( “Fringe pattern database”)
+  for (i<- fpdb) {
+    println(i._1) ;println(i._2) }
+  println( “Corner pattern database”)
+  for (i<- cpdb) {
+    println(i._1) ;println(i._2) }*/
 }
 
-def pattern(s:State): State={
+// Generate either fringe or corner pattern for input state 
+def pattern(s:State, fringe:Boolean): State={
   var ptiles:Map[Pos,Tile]=Map()
+  var sub:List[Int]=List()
   s match{
     case State(b,e)=> b match{
       case Board(size,t)=> { 
-        val fringe=List.tabulate(2*size-1)(n=>{if (n<size) n+1 else (n-2)*size+1 })
-        for (i<-1 to size){ for (j<-1 to size){
-          t get (i,j) match {
-           case None=> {}
-           case Some(tile)=>  
-            if (fringe contains tile) ptiles=ptiles+((i,j)->tile)}}}
-            State(Board(size, ptiles), e)
-     }
-   }
+       if (fringe){
+         sub=List.tabulate(2*size-1)(n=>{if (n<size) n+1 else (n-2)*size+1 })}
+       else {sub= List.tabulate(2*size-1)(n=>{if (n<size) n+1 else n+2})}
+       for (i<-1 to size){ for (j<-1 to size){
+           t get (i,j) match {
+       case None=> {}
+       case Some(tile)=>  if (sub contains tile )
+          ptiles=ptiles+((i,j)->tile)}}}
+       State(Board(size, ptiles), e)
+      }
+    }
   }
 }
 
-def fringePattern(s:State):Int={
-    val p=pattern(s)
-    patternCost get p
+// Non-additive pattern database heuristics
+// Three modes 1:fringe 2.corner 3.max(fringe,corner) 
+def nonAdditive(s:State, mode:Int):Int={
+  var heuristics=0; 
+  if (mode==0 | mode==2){
+    val p=pattern(s,true);
+    heuristics += fpdb(p);
+    if (mode==0) heuristics
+  }
+  if (mode==1 | mode==2){
+    val p2=pattern(s,false);
+    heuristics += cpdb(p2);
+    if (mode==1) heuristics
+  }
+  heuristics
 }
+
