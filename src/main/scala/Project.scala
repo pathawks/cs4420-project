986012f9badc8f063885c9862169ecf4ea4b1dbc
Pat Hawks <patrick-hawks@uiowa.edu>
2017-04-30 14:50:45 -0500

    Add (non-working) main method for project
    This will *need* to be tweaked when we get `import` working 👹

---
+++ src/main/scala/Project.scala
@@ -0,0 +1,33 @@
+/*==================================================
+    CS:4420 Artificial Intelligence
+    Spring 2017
+    Main method
+    Name: Pat Hawks, Ryan Larson, Rui Yang
+  ==================================================*/
+
+object HelloWorld {
+  /**
+   * Entry point for our program
+   * Takes three command line arguments:
+   *   0 - Filename of state to load
+   *   1 - Variant of A* to use
+   *   2 - Heuristic to use
+   */
+  def main(args: Array[String]) = {
+    val s = Utilities.read_board_to_map(args[0])
+    val search = args[1] match {
+      case "astar" => astar
+      case "id"    => astar
+      case _       => throw "Unknown search"
+    }
+    val heuristic = args[2] match {
+      case "Manhatta"       => Manhatta
+      case "linearConflict" => linearConflict
+      case "NMaxSwap"       => NMaxSwap
+      case "nonAdditive"    => nonAdditive
+      case _                => throw "Unknown Search"
+    }
+
+    search(s, goalState(3), validMoves, heuristic, costs)
+  }
+}
