f7a7e3ca57e68357e806fb9834494127913bb6a0
Pat Hawks <patrick-hawks@uiowa.edu>
2017-05-02 16:25:47 -0500

    Whitespace changes only

--- src/main/scala/Nsquare.scala
+++ src/main/scala/Nsquare.scala
@@ -30,13 +30,13 @@ object Nsquare {
       }
       new Board(size, t1)
     }
-   // convert the board contents into byte array   
-   def toBytes():Array[Byte]= {   
+   // convert the board contents into byte array
+   def toBytes():Array[Byte]= {
      var s:Array[Byte]=Array()
       for (i <- 1 to size) {
         for (j <- 1 to size){
           tiles get (i,j) match {
-            case None    => s ++= Array(' '.toByte)       
+            case None    => s ++= Array(' '.toByte)
             case Some(v) => s ++= Array(v.toByte)}
        }
      }
@@ -91,7 +91,7 @@ object Nsquare {
     }
     */
     // states are converted into bytes array of board without empty position, for non-addivtive pattern databases
-    def toBytes() = board.toBytes() 
+    def toBytes() = board.toBytes()
     // states are converted into bytes array of board followed by the empty position, for disjoint pattern databases
     def toBytes2() = board.toBytes() ++ Array(emptyPos._1.toByte) ++ Array(emptyPos._2.toByte)
   }
@@ -114,8 +114,8 @@ object Nsquare {
 */
   abstract class Operator {
     def apply(s: State): Option[State]
-    /** moving tiles operation that used for generating disjoint pattern databases, 
-      * check if this moves a tile in specific partition.  
+    /** moving tiles operation that used for generating disjoint pattern databases,
+      * check if this moves a tile in specific partition.
       * @param s State to search
       * @param list1 tiles in 'up' area of solution puzzle in horizatal partition
       * @param list2 tiles in 'left' area of solution puzzle in vertical partition
@@ -177,7 +177,7 @@ object Nsquare {
       }
     override def apply (s: State, list1:List[Int],list2:List[Int]): Option[(State,Boolean,Boolean)] =  None
   }
-    
+
   // left move operator used in disjoint pattern database generation
   case object LeftDPDB extends Operator {
     override def apply(s: State): Option[State]=None
@@ -196,8 +196,8 @@ object Nsquare {
         }
       }
   }
-    
-// right move operator used in disjoint pattern database generation    
+
+// right move operator used in disjoint pattern database generation
 case object RightDPDB extends Operator {
   override def apply(s: State): Option[State]=None
   // the apply method returns
@@ -234,7 +234,7 @@ case object UpDPDB extends Operator {
       }
     }
   }
-    
+
 // down move operator used in disjoint pattern database generation
 case object DownDPDB extends Operator {
   // the apply method returns
@@ -255,7 +255,7 @@ case object DownDPDB extends Operator {
     }
 
 }
-    
+
   type Plan = List[Operator]
 
   // given a state s and a plan p, attempt to execute p starting with s.
@@ -275,7 +275,7 @@ case object DownDPDB extends Operator {
 
   /**
     * Find a List of all States that are one Operator away from a given State s
-    * This is used as our makeNodes function for A* and the generation of non-additive pattern databases 
+    * This is used as our makeNodes function for A* and the generation of non-additive pattern databases
     *
     * @param s State to search
     * @return List of States reachable from current state, and the Operator that
@@ -288,10 +288,10 @@ case object DownDPDB extends Operator {
       case None => l
     })
   }
-    
-  /** 
+
+  /**
     * Find a list of valid new states, corresponding operator from a given state s
-    * This is used as our makeNodes function for generation of disjoint pattern databases 
+    * This is used as our makeNodes function for generation of disjoint pattern databases
     *
     * @param s State to search
     * @param list1 tiles in 'up' area of solution puzzle in horizatal partition
